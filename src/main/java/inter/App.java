c96162c994cf2b5e995dea20d7d2a0617bb6dafd
Nils Leo <nilstleo01@gmail.com>
2023-02-05 00:30:29 +0100

    first commit

---
+++ src/main/java/inter/App.java
@@ -0,0 +1,33 @@
+package inter;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            List<Card> cards = new ArrayList<>();
+
+            Card card = new Card();
+            entityManager.persist(card);
+            transaction.commit();
+        } finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
