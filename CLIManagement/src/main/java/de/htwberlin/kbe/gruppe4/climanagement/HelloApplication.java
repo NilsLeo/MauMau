492d68ab67a163242da7af6b8dcb43c0756bbead
Nils Leo <nilstleo01@gmail.com>
2023-02-05 02:04:08 +0100

    save

--- CLIManagement/src/main/java/de/htwberlin/kbe/gruppe4/climanagement/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package de.htwberlin.kbe.gruppe4.climanagement;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
