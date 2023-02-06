7e6eeaabc1bd96e86919dffd016382eab4b2ce32
Nils Leo <nilstleo01@gmail.com>
2023-02-06 23:59:55 +0100

    refactor

--- CLIManagement/src/main/java/impl/CLIControllerImpl.java
+++ CLIManagement/src/main/java/impl/CLIControllerImpl.java
@@ -34,6 +34,10 @@ public class CLIControllerImpl implements CLIController {
         gameService.setPlayers(cli.getPlayerNames());
         gameService.setRules(cli.getRule("draw two on seven"), cli.getRule("choose suit on jack"), cli.getRule("reverse on ace"));
         gameService.startGame();
+        runGame();
+    }
+
+    public void runGame(){
         while (!gameService.isGameOver()) {
             cli.displayLead(gameService.getLeadCard().getSuit(), gameService.getLeadCard().getValue());
             Player player = gameService.getPlayers().get(gameService.getCurrentPlayer());
@@ -60,58 +64,7 @@ public class CLIControllerImpl implements CLIController {
                 drawCard(player);
                 gameService.setCurrentPlayer(noOfTurns);
             } else {
-                int index = 0;
-                if (input.contains("m")) {
-                    if ((player.getHand().size() == 2)) {
-                        cli.announceMauMau();
-                        rememberedToSayMauMau = true;
-                        Matcher matcher = Pattern.compile("\\d+").matcher(input);
-                        matcher.find();
-                        index = Integer.valueOf(matcher.group())-1;
-                    } else {
-                        cli.announceInvalidMauMauCall();
-                    }
-                } else {
-
-                    index = Integer.parseInt(input)-1;
-                }
-                Card played = gameService.playCard(player, index);
-                if (played == null || !gameService.isCardValid(played, lead)) {
-                    cli.announceInvalid();
-                    playTurn(player, lead, getPlayOrDraw(), turns + 1);
-                    lead = played;
-                } else if(gameService.isCardValid(played, lead)) {
-                    cli.displayPlay(played.getSuit(), played.getValue());
-                    gameService.addCardToTable(played);
-
-                    lead = gameService.getLeadCard();
-                    if (gameService.isDrawTwoOnSeven() && played.getValue() == Value.SEVEN) {
-                        nextPlayerDraws += 2;
-                    }
-
-                    if (gameService.isChooseSuitOnJack() && played.getValue() == Value.JACK) {
-                        cli.displaySuitChoice();
-                        cli.displaySuits();
-                        Suit choice = cli.getSuitChoice();
-                        gameService.setSuitChoice(choice);
-                        cli.announceChosenSuit(choice);
-                    }
-
-                    if (gameService.isReverseOnAce() && played.getValue() == Value.ACE) {
-                        gameService.setReversed(!gameService.isReversed());
-                    }
-                    // 2 Karten Strafziehen
-                    if (player.getHand().size() == 1 && !rememberedToSayMauMau) {
-                        cli.announceForgotToSayMauMau();
-                        drawCard(player);
-
-                        drawCard(player);
-                    }
-                    if (player.getHand().size() == 1) {
-                        noOfTurns = -1;
-                    }
-                    gameService.setCurrentPlayer(noOfTurns);
-                }
+                playCard(input, player, lead, turns, noOfTurns);
             }
         } catch (Exception e) {
             cli.announceInvalid();
@@ -119,10 +72,73 @@ public class CLIControllerImpl implements CLIController {
         gameService.refillDeckwithExcessCardsOnTable();
     }
 
+    void confirmOrDenyMauMau(Player player, int index, String input){
+        if ((player.getHand().size() == 2)) {
+            cli.announceMauMau();
+            rememberedToSayMauMau = true;
+            Matcher matcher = Pattern.compile("\\d+").matcher(input);
+            matcher.find();
+            index = Integer.valueOf(matcher.group())-1;
+        } else {
+            cli.announceInvalidMauMauCall();
+        }
+    }
+
+    void applySpecialRules(Card played){
+        if (gameService.isDrawTwoOnSeven() && played.getValue() == Value.SEVEN) {
+            nextPlayerDraws += 2;
+        }
+
+        if (gameService.isChooseSuitOnJack() && played.getValue() == Value.JACK) {
+            cli.displaySuitChoice();
+            cli.displaySuits();
+            Suit choice = cli.getSuitChoice();
+            gameService.setSuitChoice(choice);
+            cli.announceChosenSuit(choice);
+        }
+
+        if (gameService.isReverseOnAce() && played.getValue() == Value.ACE) {
+            gameService.setReversed(!gameService.isReversed());
+        }
+    }
+
+    void playCard(String input, Player player, Card lead, int turns, int noOfTurns){
+        int index = 0;
+        if (input.contains("m")) {
+            confirmOrDenyMauMau(player, index, input);
+        } else {
+
+            index = Integer.parseInt(input)-1;
+        }
+        Card played = gameService.playCard(player, index);
+        if (played == null || !gameService.isCardValid(played, lead)) {
+            cli.announceInvalid();
+            playTurn(player, lead, getPlayOrDraw(), turns + 1);
+        } else if(gameService.isCardValid(played, lead)) {
+            cli.displayPlay(played.getSuit(), played.getValue());
+            gameService.addCardToTable(played);
+            applySpecialRules(played);
+            // 2 Karten Strafziehen
+            if (player.getHand().size() == 1 && !rememberedToSayMauMau) {
+                penaltyDraw(player);
+            }
+            if (player.getHand().size() == 1) {
+                noOfTurns = -1;
+            }
+            gameService.setCurrentPlayer(noOfTurns);
+        }
+    }
 
     public String getPlayOrDraw() {
         cli.displayPlayOrDraw();
         return cli.getPlayOrDraw();
+    }
+
+    public void penaltyDraw(Player player){
+        cli.announceForgotToSayMauMau();
+        drawCard(player);
+
+        drawCard(player);
     }
 
     public void drawCard(Player player){
