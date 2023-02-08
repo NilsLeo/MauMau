da8e190e8808862bcca7420a17415812c26a57f2
Nils Leo <nilstleo01@gmail.com>
2023-02-08 03:39:56 +0100

    Uebungsblatt 3 ready (tests noch nicht vollständig))

---
+++ GameManagement/src/test/java/de/htwberlin/kbe/gruppe4/impl/GameServiceImplTest.java
@@ -0,0 +1,112 @@
+package de.htwberlin.kbe.gruppe4.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceImplTest {
+
+    @Test
+    void testSetPlayers() {
+    }
+
+    @Test
+    void setRules() {
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void getLeadCard() {
+    }
+
+    @Test
+    void refillDeckwithExcessCardsOnTable() {
+    }
+
+    @Test
+    void hasCardsLeft() {
+    }
+
+    @Test
+    void getNextPlayerDraws() {
+    }
+
+    @Test
+    void setNextPlayerDraws() {
+    }
+
+    @Test
+    void getSpecialRules() {
+    }
+
+    @Test
+    void applySpecialRules() {
+    }
+
+    @Test
+    void setSuitChoice() {
+    }
+
+    @Test
+    void setDirectionClockwise() {
+    }
+
+    @Test
+    void isDirectionClockwise() {
+    }
+
+    @Test
+    void setRememberedToSayMauMau() {
+    }
+
+    @Test
+    void getRememberedToSayMauMau() {
+    }
+
+    @Test
+    void cardToPlay() {
+    }
+
+    @Test
+    void addCardToTable() {
+    }
+
+    @Test
+    void drawCard() {
+    }
+
+    @Test
+    void playCard() {
+    }
+
+    @Test
+    void setCurrentPlayer() {
+    }
+
+    @Test
+    void getPlayers() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getLeadSuit() {
+    }
+
+    @Test
+    void getLeadValue() {
+    }
+
+    @Test
+    void isGameOver() {
+    }
+
+    @Test
+    void isCardValid() {
+    }
+}
