6da4a137bcb22f9c76eab01afa7d821fb3d1e5d5
Nils Leo <nilstleo01@gmail.com>
2023-02-11 19:42:47 +0100

    save

--- GameManagement/src/main/java/de/htwberlin/kbe/gruppe4/impl/InvalidGameException.java
+++
@@ -1,9 +0,0 @@
-package de.htwberlin.kbe.gruppe4.impl;
-
-public class InvalidGameException extends Exception {
-
-    public InvalidGameException(String message) {
-        super(message);
-    }
-
-}
